08e7eecaab80615e639a6fcc890a6d82ed1fd230
Daleel Hagy <daleel.n.hagy@gmail.com>
2020-03-01 03:56:50 +0000

    adding title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Next Of Kin',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Next of Kin talents and projects',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
