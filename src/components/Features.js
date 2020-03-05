1ebc3de3d2f232efe1e198b20ef4066020c6a3bb
Daleel Hagy <daleel.n.hagy@gmail.com>
2020-03-05 00:33:25 +0000

    update projects

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import PreviewCompatibleImage from "../components/ProjectsImage";
+import "./Features.css";
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
+  <div className="">
     {gridItems.map(item => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
-            </div>
-          </div>
+      <div key={item.text} className="project">
+        {/* <section className=""> */}
+        {/* <div className=""> */}
+        <div className="">
+          <PreviewCompatibleImage imageInfo={item} />
+        </div>
+        {/* </div> */}
+        <div className="projectText">
           <p>{item.text}</p>
-        </section>
+        </div>
+        {/* </section> */}
       </div>
     ))}
   </div>
-)
+);
 
 FeatureGrid.propTypes = {
   gridItems: PropTypes.arrayOf(
     PropTypes.shape({
       image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-      text: PropTypes.string,
+      text: PropTypes.string
     })
-  ),
-}
+  )
+};
 
-export default FeatureGrid
+export default FeatureGrid;
