2150176a866b72fdfd975410e76ff183686a050a
Daleel Hagy <daleel.n.hagy@gmail.com>
2020-03-24 11:30:35 +0000

    adding margin

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -27,67 +27,77 @@ export default class Index extends React.Component {
         <div className="contactMain">
           <h1 className="contactHeader">Contact</h1>
           <div className="gridContact">
-            <div className="socialInfo">
-              <a href="tel:+442035596554">+44 20 355 96554</a>
-              <br />
-              <br />
-              <a href="mailto:info@nextofkin.com">someone@nextofkin.com</a>
-              <br />
-              <br />
-              <a
-                href="https://www.google.co.uk/maps/place/Code+Your+Future/@51.5329326,-0.0591323,17z/data=!3m1!4b1!4m5!3m4!1s0x48761dd12bb927a3:0xf3e50cf1e0d3a1a4!8m2!3d51.5329326!4d-0.0569436"
-                target="_blank"
-              >
-                <address className="address">
-                  The Green House<br></br>
-                  244-254 Cambridge Heath Rd<br></br>
-                  London<br></br>
-                  E2 9DA<br></br>
-                </address>
-              </a>
+            <div className="gridContactItem">
+              <div className="socialInfo">
+                <a href="tel:+442035596554">+44 20 355 96554</a>
+                <br />
+                <br />
+                <a href="mailto:info@nextofkin.com">someone@nextofkin.com</a>
+                <br />
+                <br />
+                <a
+                  href="https://www.google.co.uk/maps/place/Code+Your+Future/@51.5329326,-0.0591323,17z/data=!3m1!4b1!4m5!3m4!1s0x48761dd12bb927a3:0xf3e50cf1e0d3a1a4!8m2!3d51.5329326!4d-0.0569436"
+                  target="_blank"
+                >
+                  <address className="address">
+                    The Green House<br></br>
+                    244-254 Cambridge Heath Rd<br></br>
+                    London<br></br>
+                    E2 9DA<br></br>
+                  </address>
+                </a>
+              </div>
             </div>
-            <div className="socialPic">
-              <img src={conatctImage} alt="office Building" />
+            <div className="gridContactItem">
+              <div className="socialPic">
+                <img src={conatctImage} alt="office Building" />
+              </div>
             </div>
           </div>
           <h1 className="contactHeader">Social</h1>
           <div className="gridContact">
-            <div className="socialIcons">
-              <div className="socialIcon">
-                <a title="facebook" href="https://facebook.com">
-                  <img className="socialImag" src={facebook} alt="Facebook" />{" "}
-                  Facebook
-                </a>
-              </div>
-              <div className="socialIcon">
-                <a title="twitter" href="https://twitter.com">
-                  <img className="socialImag" src={twitter} alt="Twitter" />
-                  {"   "}
-                  Twitter
-                </a>
-              </div>
-              <div className="socialIcon">
-                <a title="instagram" href="https://instagram.com">
-                  <img className="socialImag" src={instagram} alt="Instagram" />{" "}
-                  Instagram
-                </a>
-              </div>
-              <div className="socialIcon">
-                <a title="vimeo" href="https://vimeo.com">
-                  <img className="socialImag" src={vimeo} alt="Vimeo" /> Vimeo
-                </a>
-              </div>
-              <div className="socialIcon">
-                <a title="youtube" href="https://youtube.com">
-                  <img className="socialImag" src={youtube} alt="Youtube" />{" "}
-                  Youtube
-                </a>
-              </div>
-              <div className="socialIcon">
-                <a title="linkedin" href="https://linkedin.com">
-                  <img className="socialImag" src={linkedin} alt="Linkedin" />{" "}
-                  Linkedin
-                </a>
+            <div className="gridContactItem">
+              <div className="socialIcons">
+                <div className="socialIcon">
+                  <a title="facebook" href="https://facebook.com">
+                    <img className="socialImag" src={facebook} alt="Facebook" />{" "}
+                    Facebook
+                  </a>
+                </div>
+                <div className="socialIcon">
+                  <a title="twitter" href="https://twitter.com">
+                    <img className="socialImag" src={twitter} alt="Twitter" />
+                    {"   "}
+                    Twitter
+                  </a>
+                </div>
+                <div className="socialIcon">
+                  <a title="instagram" href="https://instagram.com">
+                    <img
+                      className="socialImag"
+                      src={instagram}
+                      alt="Instagram"
+                    />{" "}
+                    Instagram
+                  </a>
+                </div>
+                <div className="socialIcon">
+                  <a title="vimeo" href="https://vimeo.com">
+                    <img className="socialImag" src={vimeo} alt="Vimeo" /> Vimeo
+                  </a>
+                </div>
+                <div className="socialIcon">
+                  <a title="youtube" href="https://youtube.com">
+                    <img className="socialImag" src={youtube} alt="Youtube" />{" "}
+                    Youtube
+                  </a>
+                </div>
+                <div className="socialIcon">
+                  <a title="linkedin" href="https://linkedin.com">
+                    <img className="socialImag" src={linkedin} alt="Linkedin" />{" "}
+                    Linkedin
+                  </a>
+                </div>
               </div>
             </div>
           </div>
