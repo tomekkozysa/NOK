b0b503ab3f0580324e040a3d6077e7cef96ebf3e
Tomasz Kozysa <tomek@15nine.com>
2020-05-01 14:51:02 +0100

    about page updates

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,44 +3,98 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import "./about.css";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
 
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
 
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func
-};
 
+// export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+
+//   const PageContent = contentComponent || Content;
+
+//   return (
+//     <section className="about-page">
+      
+//       <h2 className="about-page-title">
+//                 {title}
+//       </h2>
+
+//       <div class="about-page-intro">
+//         <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+//         <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+//       </div>
+
+
+//       {/* <PageContent className="about-page-content" content={content} /> */}
+
+
+//     </section>
+//   );
+// };
+
+// AboutPageTemplate.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   content: PropTypes.string,
+//   contentComponent: PropTypes.func
+// };
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data;
 
   return (
     <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
+
+
+<section className="about-page">
+      
+      <h2 className="about-page-title">
+               About
+      </h2>
+
+      <div className="about-page-intro">
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+
+      <div className="about-page-profiles">
+        
+        <div className="about-page-profile">
+          <div className="profile-img-wrapper">
+            <img className="profile-img" src={'/img/gr-orange-pink.png'} alt="Logo" />
+          </div>
+          <div className="about-page-profile-text">
+          <p className="about-page-profile-text-copy">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+
+        <div className="about-page-profile">
+          <div className="profile-img-wrapper">
+            <img className="profile-img" src={'/img/gr-orange-pink.png'} alt="Logo" />
+          </div>
+          <div className="about-page-profile-text">
+          <p className="about-page-profile-text-copy">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+
+        <div className="about-page-profile">
+          <div className="profile-img-wrapper">
+            <img className="profile-img" src={'/img/gr-orange-pink.png'} alt="Logo" />
+          </div>
+          <div className="about-page-profile-text">
+          <p className="about-page-profile-text-copy">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+
+
+      </div>
+      
+      
+
+    </section>
+
+
+
+      
+
+
     </Layout>
   );
 };
