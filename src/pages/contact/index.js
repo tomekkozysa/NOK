7a130a1cc4f88c170bf64447ca21d2ed49d2ecc0
Tomasz Kozysa <tomek@15nine.com>
2020-05-01 14:52:46 +0100

    contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import Layout from "../../components/Layout";
 import "./contact.css";
 import conatctImage from "../../../static/img/contact.jpg";
-import facebook from "../../img/social/facebook.svg";
-import instagram from "../../img/social/instagram.svg";
-import twitter from "../../img/social/twitter.svg";
-import vimeo from "../../img/social/vimeo.svg";
-import youtube from "../../img/social/youtube.svg";
-import linkedin from "../../img/social/linkedin.svg";
+
 
 function encode(data) {
   return Object.keys(data)
@@ -24,83 +19,32 @@ export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="contactMain">
-          <h1 className="contactHeader">Contact</h1>
-          <div className="gridContact">
-            <div className="gridContactItem">
-              <div className="socialInfo">
-                <a href="tel:+442035596554">+44 20 355 96554</a>
-                <br />
-                <br />
-                <a href="mailto:info@nextofkin.com">someone@nextofkin.com</a>
-                <br />
-                <br />
-                <a
-                  href="https://www.google.co.uk/maps/place/Code+Your+Future/@51.5329326,-0.0591323,17z/data=!3m1!4b1!4m5!3m4!1s0x48761dd12bb927a3:0xf3e50cf1e0d3a1a4!8m2!3d51.5329326!4d-0.0569436"
-                  target="_blank"
-                >
-                  <address className="address">
-                    The Green House<br></br>
-                    244-254 Cambridge Heath Rd<br></br>
-                    London<br></br>
-                    E2 9DA<br></br>
-                  </address>
-                </a>
-              </div>
-            </div>
-            <div className="gridContactItem">
-              <div className="socialPic">
-                {/* <img src={conatctImage} alt="office Building" /> */}
-              </div>
-            </div>
-          </div>
-          <h1 className="contactHeader">Social</h1>
-          <div className="gridContact">
-            <div className="gridContactItem">
-              <div className="socialIcons">
-                <div className="socialIcon">
-                  <a title="facebook" href="https://facebook.com">
-                    <img className="socialImag" src={facebook} alt="Facebook" />{" "}
-                    Facebook
-                  </a>
-                </div>
-                <div className="socialIcon">
-                  <a title="twitter" href="https://twitter.com">
-                    <img className="socialImag" src={twitter} alt="Twitter" />
-                    {"   "}
-                    Twitter
-                  </a>
-                </div>
-                <div className="socialIcon">
-                  <a title="instagram" href="https://instagram.com">
-                    <img
-                      className="socialImag"
-                      src={instagram}
-                      alt="Instagram"
-                    />{" "}
-                    Instagram
-                  </a>
-                </div>
-                <div className="socialIcon">
-                  <a title="vimeo" href="https://vimeo.com">
-                    <img className="socialImag" src={vimeo} alt="Vimeo" /> Vimeo
-                  </a>
-                </div>
-                <div className="socialIcon">
-                  <a title="youtube" href="https://youtube.com">
-                    <img className="socialImag" src={youtube} alt="Youtube" />{" "}
-                    Youtube
-                  </a>
-                </div>
-                <div className="socialIcon">
-                  <a title="linkedin" href="https://linkedin.com">
-                    <img className="socialImag" src={linkedin} alt="Linkedin" />{" "}
-                    Linkedin
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="contact-page">
+          <h2 className="contact-page-headline">Contact</h2>
+          <ul className="contact-page-primary-details">
+            <li className="contact-page-detail">
+            <a href="tel:+442035596554">+44 20 355 96554</a>
+            </li>
+            <li className="contact-page-detail">
+            <a href="mailto:info@nextofkin.com">someone@nextofkin.com</a>
+            </li>
+            
+          </ul>
+          <address  className="contact-page-detail contact-address">
+                The Green House <br />
+                244-254 Cambridge Heath Rd<br />
+                London<br />
+                E2 9DA<br />
+            </address>
+
+          <h2 className="contact-page-headline social-headline">Social</h2>
+          <ul className="social-links">
+            <li className="social-link">facebook</li>
+            <li className="social-link">twitter</li>
+            <li className="social-link">instagram</li>
+            <li className="social-link">youtube</li>
+            <li className="social-link">linkedin</li>
+          </ul>
         </div>
       </Layout>
     );
