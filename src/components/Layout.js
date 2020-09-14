ed1b91e1b79f814080846b11f1bb6b04a2bac48e
Vicky <kosenchiha@gmail.com>
2020-09-14 15:24:28 +0100

    Add meta links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -20,48 +20,44 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon.png`}
+          href={`${withPrefix("/")}img/icons/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
+          href={`${withPrefix("/")}img/icons/favicon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
+          href={`${withPrefix("/")}img/icons/favicon-16x16.png`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
+          href={`${withPrefix("/")}img/icons/safari-pinned-tab.svg`}
           color="#ff4400"
         />
 
         <meta name="theme-color" content="#fff" />
         <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" 
-        
-        
-        content={`${withPrefix("/")}img/og-image.jpg`}
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content={`${withPrefix("/")}img/icons/nok-social.jpg`}
         />
 
-        
-
-
+        <meta property="twitter:card" content="summary_large_image" />
       </Helmet>
 
       <Header />
-      
-      <div className="layout-content">
-        {children}
-        
-      </div>
+
+      <div className="layout-content">{children}</div>
       {/* <Footer /> */}
     </div>
   );
