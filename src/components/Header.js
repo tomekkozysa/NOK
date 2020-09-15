dddf3b9c08d15f65fdbf09809f77be5a77b66340
Tomasz Kozysa <tomek@15nine.com>
2020-09-15 12:50:01 +0100

    clickable logo, new nav icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
-import nextofkin from "../img/nextofkin.svg";
-import "./Header.css";
+import { Link } from "gatsby";
+
 import Navigation from "../components/Navigation";
+
+import "./Header.css";
 
 
 const Header = class extends React.Component {
@@ -39,16 +41,17 @@ const Header = class extends React.Component {
     return (
       <header className="header-main">
         <hgroup className="header-content">
-          <div className="header-logo">
-            <img src={nextofkin} alt="NextOfKin" />
-          </div>
+          <h1 className="header-logo">
+            
+            <Link className="header-logo-link" to="/">Next of Kin</Link>            
+          </h1>
           <div className="nav-toggle">
             <svg className={this.state.active ? "nav-toggle-button is_open" : "nav-toggle-button"} 
             onClick={() => this.toggleHamburger()}            
-            width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 
-            <line className="nav-toggle-button-line-one" x1="0" y1="15" y2="15" x2="30" strokeWidth="3" stroke="var(--c-primary)" />
-            <line className="nav-toggle-button-line-two" x1="0" y1="15" y2="15" x2="30" strokeWidth="3" stroke="var(--c-primary)" />
+            <line className="nav-toggle-button-line-one" x1="0" y1="12.5" y2="12.5" x2="25" strokeWidth="4" stroke="var(--c-primary)" />
+            <line className="nav-toggle-button-line-two" x1="0" y1="12.5" y2="12.5" x2="25" strokeWidth="4" stroke="var(--c-primary)" />
             </svg>      
           </div>
         </hgroup>
