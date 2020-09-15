59983f92412bc03abe123c9ac5b57712e7cc89e5
Vicky <kosenchiha@gmail.com>
2020-09-15 15:09:07 +0100

    Fix navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,36 +3,32 @@ import nextofkin from "../img/nextofkin.svg";
 import "./Header.css";
 import Navigation from "../components/Navigation";
 
-
 const Header = class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       active: false,
-      navBarActiveClass: ""
+      navBarActiveClass: "",
     };
   }
 
   toggleHamburger = () => {
-
     this.setState(
       {
-        active: !this.state.active
+        active: !this.state.active,
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-            navBarActiveClass: "is_active"
-          })
+              navBarActiveClass: "is_active",
+            })
           : this.setState({
-            navBarActiveClass: ""
-          });
+              navBarActiveClass: "",
+            });
       }
     );
-
-    
   };
 
   render() {
@@ -43,18 +39,45 @@ const Header = class extends React.Component {
             <img src={nextofkin} alt="NextOfKin" />
           </div>
           <div className="nav-toggle">
-            <svg className={this.state.active ? "nav-toggle-button is_open" : "nav-toggle-button"} 
-            onClick={() => this.toggleHamburger()}            
-            width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-            <line className="nav-toggle-button-line-one" x1="0" y1="15" y2="15" x2="30" strokeWidth="3" stroke="var(--c-primary)" />
-            <line className="nav-toggle-button-line-two" x1="0" y1="15" y2="15" x2="30" strokeWidth="3" stroke="var(--c-primary)" />
-            </svg>      
+            <svg
+              className={
+                this.state.active
+                  ? "nav-toggle-button is_open"
+                  : "nav-toggle-button"
+              }
+              onClick={() => this.toggleHamburger()}
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <line
+                className="nav-toggle-button-line-one"
+                x1="0"
+                y1="15"
+                y2="15"
+                x2="30"
+                strokeWidth="3"
+                stroke="var(--c-primary)"
+              />
+              <line
+                className="nav-toggle-button-line-two"
+                x1="0"
+                y1="15"
+                y2="15"
+                x2="30"
+                strokeWidth="3"
+                stroke="var(--c-primary)"
+              />
+            </svg>
           </div>
         </hgroup>
-        <Navigation active={this.state.navBarActiveClass} />
+        <Navigation
+          active={this.state.navBarActiveClass}
+          toggleHamburger={this.toggleHamburger}
+        />
       </header>
-
     );
   }
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,43 @@
 import React from "react";
 import { Link } from "gatsby";
-import nextofkin from "../img/nextofkin.svg";
-import navbutton from "../img/navbutton.svg";
 import "./Navigation.css";
 
-const Navigation = class extends React.Component {
+const Navigation = ({ active, toggleHamburger }) => {
+  return (
+    <nav role="main" className={`navigation ${active}`}>
+      <ul className="navigation-list">
+        <li className="navigation-list-item">
+          <Link
+            onClick={toggleHamburger}
+            className="navigation-list-link"
+            to="/"
+          >
+            work
+          </Link>
+        </li>
 
+        <li className="navigation-list-item">
+          <Link
+            onClick={toggleHamburger}
+            className="navigation-list-link"
+            to="/about"
+          >
+            about
+          </Link>
+        </li>
 
-  render() {
-    return (
-
-
-      <nav role="main" className={`navigation ${this.props.active}`}>
-
-        <ul className="navigation-list">
-
-          <li className="navigation-list-item">
-            <Link className="navigation-list-link" to="/">work</Link>
-          </li>
-
-          <li className="navigation-list-item">
-            <Link className="navigation-list-link" to="/about">about</Link>
-          </li>
-          
-          <li className="navigation-list-item">
-            <Link className="navigation-list-link" to="/contact">contact</Link>
-          </li>
-
-          
-
-        </ul>
-      </nav>
-    );
-  }
+        <li className="navigation-list-item">
+          <Link
+            className="navigation-list-link"
+            to="/contact"
+            onClick={toggleHamburger}
+          >
+            contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
