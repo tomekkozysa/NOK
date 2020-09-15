f234959468204706857ed70b87cae5ad90f97c0b
reyamme <47262195+reyamme@users.noreply.github.com>
2020-09-15 18:22:17 +0100

    editing the footer file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,105 +12,10 @@ import "./Footer.css";
 const Footer = class extends React.Component {
   render() {
     return (
-      // <footer className="footer has-text-white-ter">
       <footer className="footer-main">
         <div>
           <p className="copy-right">&copy;Next of Kin 2020. This website was built by graduates from <a href="https://codeyourfuture.io">CodeYourFuture</a></p>
         </div>
-        {/* <div className="content has-text-centered"> */}
-        {/* <div className="header-logo">
-            <img src={nextofkin} alt="NextOfKin" />
-          </div> */}
-        {/* </div> */}
-        {/* <div className="content has-text-centered has-text-white-ter">
-          <div className="container has-text-white-ter"> */}
-        {/* <div className="columns">
-              <div className="column is-4"> */}
-        {/* <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section> */}
-        {/* </div> */}
-        {/* <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div> */}
-        {/* <div className="column is-4 social">
-          <a title="facebook" href="https://facebook.com">
-            <img
-              src={facebook}
-              alt="Facebook"
-              style={{ width: "1em", height: "1em" }}
-            />
-          </a>
-          <a title="twitter" href="https://twitter.com">
-            <img
-              className="fas fa-lg"
-              src={twitter}
-              alt="Twitter"
-              style={{ width: "1em", height: "1em" }}
-            />
-          </a>
-          <a title="instagram" href="https://instagram.com">
-            <img
-              src={instagram}
-              alt="Instagram"
-              style={{ width: "1em", height: "1em" }}
-            />
-          </a>
-          <a title="vimeo" href="https://vimeo.com">
-            <img
-              src={vimeo}
-              alt="Vimeo"
-              style={{ width: "1em", height: "1em" }}
-            />
-          </a>
-        </div> */}
-        {/* </div> */}
-        {/* // </div> */}
-        {/* </div> */}
       </footer>
     );
   }
