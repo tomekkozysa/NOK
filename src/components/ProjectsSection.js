8f8f1a148a75ac56a11bc2c50701733c08437fe7
Tomasz Kozysa <tomek@15nine.com>
2020-09-15 21:43:02 +0100

    scroll up behaviour and position sticky layout fixes

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Img from "gatsby-image";
 const CATEGORIES = ["TV and Theatre", "Short Form and Commercial", "Films"];
 
@@ -8,7 +8,8 @@ const ProjectsSection = ({ projects }) => {
   const [startAnimation, setStartAnimation] = useState(false);
 
   const handleFilterChoice = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    scrollUp();
     const filterParam = event.target.id.toLowerCase();
     if (filterParam === filter) {
       setFilter(null);
@@ -33,50 +34,62 @@ const ProjectsSection = ({ projects }) => {
       : className;
   };
 
-  return (
-    <section className="home-page-projects">
-      <div className="home-page-projects-filter">
-        <h2 className="projects-filter-headline">Our work</h2>
-        <ul className="projects-filter-items">
-          {CATEGORIES.map((category) => (
-            
-            <li
-              key={category}             
-            >
-              <a href="#" 
-              id={category}
-              className={getClassForFilterItem(category, filter)}
-              onClick={handleFilterChoice}
-              >{category}
+  const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop-200)   
+  const allProjects = useRef(null)
+  const scrollUp = () => scrollToRef(allProjects)
 
-              </a>
-            </li>
+
+  return (
+    <section>
+        <hr className="projects-filter-ruler" role="presentation" />
+        <h2 className="projects-filter-headline">Our work</h2>
+        <div className="projects-filter-items">
+          <ul className="projects-filter-items-list">
+              {CATEGORIES.map((category) => (
+                
+                <li
+                  key={category}             
+                >
+                  <a href="#" 
+                  id={category}
+                  className={getClassForFilterItem(category, filter)}
+                  onClick={handleFilterChoice}
+                  >{category}
+                  </a>
+                </li>
+              ))}
+            </ul>
+        </div>        
+      <section className="home-page-projects" ref={allProjects}>
+        {/* <div className="home-page-projects-filter">
+        
+          
+        </div> */}
+        
+        <div
+          className={startAnimation ? "nok-projects-wrapper" : ""}
+          onAnimationEnd={() => setStartAnimation(false)}
+        >
+          {projectsToDisplay.map(({ frontmatter: project }, index) => (
+            <div key={index} className="nok-project isnot_expanded">
+              <div className="project-img-wrapper">
+                <Img
+                  fluid={project.image.childImageSharp.fluid}
+                  alt={project.title}
+                  placeholderStyle = {{opacity:0}}
+                  backgroundColor = '#333'
+                />
+              </div>
+              <div className="nok-project-text">
+                <p className="nok-project-category">{project.category}</p>
+                <h3 className="nok-project-heading">{project.title}</h3>
+                <p className="nok-project-text-copy">{project.description}</p>
+              </div>
+            </div>
           ))}
-        </ul>
-      </div>
-      <div
-        className={startAnimation ? "nok-projects-wrapper" : ""}
-        onAnimationEnd={() => setStartAnimation(false)}
-      >
-        {projectsToDisplay.map(({ frontmatter: project }, index) => (
-          <div key={index} className="nok-project isnot_expanded">
-            <div className="project-img-wrapper">
-              <Img
-                fluid={project.image.childImageSharp.fluid}
-                alt={project.title}
-                placeholderStyle = {{opacity:0}}
-                backgroundColor = '#333'
-              />
-            </div>
-            <div className="nok-project-text">
-              <p className="nok-project-category">{project.category}</p>
-              <h3 className="nok-project-heading">{project.title}</h3>
-              <p className="nok-project-text-copy">{project.description}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      </section>
+    </section> 
   );
 };
 
