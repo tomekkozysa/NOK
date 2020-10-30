66d27408bb8f467b5316cedea8eb650c2c74ca0c
Tomasz Kozysa <tomek@15nine.com>
2020-10-30 15:51:46 +0000

    copyrights formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer-main">
         <div className="footer-copy">
-          <span className="footer-copyrights">&copy;Next of Kin 2020 </span>
+          <span className="footer-copyrights">&copy; Next of Kin 2020 </span>
           <span className="footer-grads">This website was built by graduates from <a href="https://codeyourfuture.io">CodeYourFuture</a></span>
         </div>
       </footer>
