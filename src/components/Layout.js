002ae1a1e579afdd01bbb81f1aab017da8c7855b
tomekkozysa <tk.rbrn@gmail.com>
2021-07-08 11:30:26 +0100

    responsive video

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -62,6 +62,7 @@ const TemplateWrapper = ({ children, path }) => {
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" ></link>
+        <script src="https://player.vimeo.com/api/player.js"></script>
 
 
       </Helmet>
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React,{useState, useRef, useEffect} from "react";
 import "./player.css";
-
+import { Helmet } from "react-helmet";
 
 
 const Player = ({id, closeClick}) => {
@@ -10,10 +10,21 @@ const Player = ({id, closeClick}) => {
     return ( 
 
         <div className="player">
+            <Helmet>
+            <script src="https://player.vimeo.com/api/player.js"></script>
+            </Helmet>
+
             <div className="player-frame">
-                <div className="player-frame-ratio">
+                {/* <div className="player-frame-ratio">
                 <iframe src={"https://player.vimeo.com/video/"+id}  width="640" height="564" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
-                </div>
+                </div> */}
+
+                    <div style={{padding:'56.25% 0 0 0', position:'relative'}}>
+                    <iframe src={'https://player.vimeo.com/video/'+id+'?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'} 
+                    style={{position:'absolute', top:0, left:0, width:'100%', height:'100%'}} 
+                    frameBorder="0" 
+                    allow="autoplay; fullscreen" allowFullScreen></iframe>
+                          </div>
             
 
             <svg
